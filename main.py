2cd79621bba11307303ee0b55787585a72d98082
sanych2891 <sanych2891@icloud.com>
2024-06-18 04:21:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+print("Количество символов равно",len('привет мир'))
+
+first = 15
+second = 23
+print("Сумма чисел равна",sum([first,second]))
+
+first=15
+second=23
+diff=first-second
+print("Разность чисел равна",diff)
+
+first=14
+second=28
+third=18
+avg = (first+second+third)/3
+print("Среднее число равно",avg)
+
+first_string="Вторник"
+second_string="Понедельник"
+print(second_string,", ",first_string)
+
+a=4
+b=5
+c=6
+f=(a*b)+(a*c)
+print("Результат:",f**3/2)
