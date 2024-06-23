1d58fc959180922d88443693f3b2547d5c618ff0
sanych2891 <sanych2891@icloud.com>
2024-06-23 15:12:44 +1000

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+# Вводим переменные для задания:
+name_of_course = 'Python'
+number_of_completed_HW = 12
+number_of_completed_HW = float(number_of_completed_HW)
+hours_spent = 3/2
+number_of_completed_HW = float(number_of_completed_HW)
+time_for_one_task = hours_spent / number_of_completed_HW
+# Вывод на экран результата:
+print("Курс: ",name_of_course,", ","всего задач:",number_of_completed_HW,", ","затрачено часов:",hours_spent,", ","среднее время выполнения",time_for_one_task," часа.")
